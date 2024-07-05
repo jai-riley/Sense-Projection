5411780fb037be0f0bbf86d4f9d8ee942d49a468
jai-riley <145622900+jai-riley@users.noreply.github.com>
2024-07-04 20:12:40 -0600

    added data including translations from English to Spanish & Italian

---
+++ src/order.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+# Load the TSV file
+input_file = '/Users/jairiley/Desktop/Research/Sense-Projection/src/main/res/results/tokens_aligned-Italian.tsv'
+output_file = '/Users/jairiley/Desktop/Research/Sense-Projection/src/main/res/results/tokens_aligned-Italian_sorted.tsv'
+
+# Read the TSV file into a DataFrame
+df = pd.read_csv(input_file, sep='\t')
+
+# Sort the DataFrame by 'Token ID ES'
+df_sorted = df.sort_values(by='Token ID IT')
+
+# Write the sorted DataFrame to a new TSV file
+df_sorted.to_csv(output_file, sep='\t', index=False)
+
+print(f'Successfully sorted and saved to {output_file}')
